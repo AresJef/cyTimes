dcd5a643ef46f55a377858b29555998cec515ee9
MacbookPro-14 <keppa1991@163.com>
2024-03-04 14:16:06 +0800

    Added error module.

--- src/cytimes/errors.py
+++ src/cytimes/errors.py
@@ -1,3 +1,6 @@
+from pandas.errors import OutOfBoundsDatetime
+
+
 # Base Exceptions ---------------------------------------------------------------------------------
 class cyTimesError(Exception):
     """The base error for the cyTimes package."""
@@ -20,7 +23,7 @@ class cyParserValueError(cyParserError, cyTimesValueError):
     """The base ValueError for cyParser module."""
 
 
-class InvalidTimestrError(cyParserValueError):
+class InvalidDatetimeStrError(cyParserValueError):
     """Error for invalid 'timestr' to parse."""
 
 
@@ -48,3 +51,52 @@ class InvalidConfigKeyword(cyParserValueError):
 class InvalidConfigValue(cyParserValueError):
     """Error for the 'cyparser.Configs' when the value
     for a keyword is invalid"""
+
+
+# Pydt/Pddt Exceptions ----------------------------------------------------------------------------
+class DatetimeError(cyTimesError):
+    """The base error for the datetime module."""
+
+
+class PydtError(DatetimeError):
+    """The base error for the pydt module."""
+
+
+class PddtError(DatetimeError):
+    """The base error for the pddt module."""
+
+
+class PydtValueError(PydtError, cyTimesValueError):
+    """The base ValueError for pydt module."""
+
+
+class PddtValueError(PddtError, cyTimesValueError):
+    """The base ValueError for pddt module."""
+
+
+class InvalidDatetimeObjectError(PydtValueError, PddtValueError):
+    """Error for invalid 'dtobj' to create a pydt object."""
+
+
+class DatetimesOutOfBounds(InvalidDatetimeObjectError, OutOfBoundsDatetime):
+    """Error for 'dtsobj' that has datetimes out of bounds."""
+
+
+class InvalidMonthError(PydtValueError, PddtValueError):
+    """Error for invalid month value."""
+
+
+class InvalidWeekdayError(PydtValueError, PddtValueError):
+    """Error for invalid weekday value."""
+
+
+class InvalidTimezoneError(PydtValueError, PddtValueError):
+    """Error for invalid timezone value."""
+
+
+class InvalidFrequencyError(PydtValueError, PddtValueError):
+    """Error for invalid frequency value."""
+
+
+class InvalidDeltaUnitError(PydtValueError, PddtValueError):
+    """Error for invalid delta unit value."""
