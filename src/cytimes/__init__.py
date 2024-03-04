eab164c0926adffdfaaf455d8d05b4644ea42467
MacbookPro-14 <keppa1991@163.com>
2024-03-04 16:09:54 +0800

    Refactor cytimes module imports.

--- src/cytimes/__init__.py
+++ src/cytimes/__init__.py
@@ -1,35 +1,73 @@
 # /usr/bin/python
 # -*- coding: UTF-8 -*-
-from cytimes.pydt import pydt, PydtValueError
-from cytimes.pddt import pddt, PddtValueError, PddtOutOfBoundsDatetime
-from cytimes.cytimedelta import Weekday, cytimedelta
-from cytimes.cyparser import TimeLex, ParserInfo, Parser, parse
+from cytimes.cyparser import Config, Parser
+from cytimes.cytimedelta import cytimedelta
+from cytimes.pydt import pydt
+from cytimes.pddt import pddt
+from cytimes.errors import (
+    # . base exceptions
+    cyTimesError,
+    cyTimesValueError,
+    # . cyparser exceptions
+    cyParserError,
+    cyParserFailedError,
+    cyParserValueError,
+    InvalidDatetimeStrError,
+    InvalidTokenError,
+    InvalidNumericToken,
+    InvalidMonthToken,
+    InvalidParserInfo,
+    InvalidConfigKeyword,
+    InvalidConfigValue,
+    # . pydt/pddt Exceptions
+    DatetimeError,
+    PydtError,
+    PddtError,
+    PydtValueError,
+    PddtValueError,
+    InvalidDatetimeObjectError,
+    DatetimesOutOfBounds,
+    InvalidMonthError,
+    InvalidWeekdayError,
+    InvalidTimezoneError,
+    InvalidFrequencyError,
+    InvalidDeltaUnitError,
+)
 
 __all__ = [
-    # Classes
+    # Class
+    "Config",
+    "Parser",
+    "cytimedelta",
     "pydt",
     "pddt",
-    "Weekday",
-    "cytimedelta",
-    "TimeLex",
-    "ParserInfo",
-    "Parser",
-    # Functions
-    "parse",
-    # Exceptions
+    # Exception
+    # . base exceptions
+    "cyTimesError",
+    "cyTimesValueError",
+    # . cyparser exceptions
+    "cyParserError",
+    "cyParserFailedError",
+    "cyParserValueError",
+    "InvalidDatetimeStrError",
+    "InvalidTokenError",
+    "InvalidNumericToken",
+    "InvalidMonthToken",
+    "InvalidParserInfo",
+    "InvalidConfigKeyword",
+    "InvalidConfigValue",
+    # . pydt/pddt Exceptions
+    "DatetimeError",
+    "PydtError",
+    "PddtError",
     "PydtValueError",
     "PddtValueError",
-    "PddtOutOfBoundsDatetime",
+    "InvalidDatetimeObjectError",
+    "DatetimesOutOfBounds",
+    "InvalidMonthError",
+    "InvalidWeekdayError",
+    "InvalidTimezoneError",
+    "InvalidFrequencyError",
+    "InvalidDeltaUnitError",
 ]
-(
-    # Classes
-    pydt,
-    pddt,
-    Weekday,
-    cytimedelta,
-    TimeLex,
-    ParserInfo,
-    Parser,
-    # Functions
-    parse,
-)  # pyflakes
+(Config, Parser, cytimedelta, pydt, pddt)  # pyflakes
