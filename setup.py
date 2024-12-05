78d961438380d9d5c4b75a999841d2e671895473
MacbookPro-14 <keppa1991@163.com>
2024-12-05 11:46:26 +0800

    refactor setup.py: improve extension function for better clarity and structure

--- setup.py
+++ setup.py
@@ -7,43 +7,59 @@ __package__ = "cytimes"
 
 
 # Create Extension
-def extension(filename: str, include_np: bool, *extra_compile_args: str) -> Extension:
+def extension(src: str, include_np: bool, *extra_compile_args: str) -> Extension:
+    # Prep name
+    if "/" in src:
+        folders: list[str] = src.split("/")
+        file: str = folders.pop(-1)
+    else:
+        folders: list[str] = []
+        file: str = src
+    if "." in file:  # . remove extension
+        file = file.split(".")[0]
+    name = ".".join([__package__, *folders, file])
+
+    # Prep source
+    if "/" in src:
+        file = src.split("/")[-1]
+    else:
+        file = src
+    source = os.path.join("src", __package__, *folders, file)
+
     # Extra arguments
     extra_args = list(extra_compile_args) if extra_compile_args else None
-    # Name
-    name: str = "%s.%s" % (__package__, filename.split(".")[0])
-    source: str = os.path.join("src", __package__, filename)
+
     # Create extension
     if include_np:
         return Extension(
             name,
-            sources=[source],
+            [source],
             extra_compile_args=extra_args,
             include_dirs=[np.get_include()],
             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         )
     else:
-        return Extension(name, sources=[source], extra_compile_args=extra_args)
+        return Extension(name, [source], extra_compile_args=extra_args)
 
 
 # Build Extensions
 if platform.system() == "Windows":
     extensions = [
-        extension("cydatetime.py", True),
-        extension("cydelta.py", True),
-        extension("cyparser.py", True),
-        extension("pddatetime.py", True),
-        extension("pydatetime.py", True),
+        extension("delta.py", True),
+        extension("parser.py", True),
+        extension("pddt.py", True),
+        extension("pydt.py", True),
         extension("typeref.py", False),
+        extension("utils.py", True),
     ]
 else:
     extensions = [
-        extension("cydatetime.py", True, "-Wno-unreachable-code", "-Wno-sign-compare"),
-        extension("cydelta.py", True, "-Wno-unreachable-code"),
-        extension("cyparser.py", True, "-Wno-unreachable-code", "-Wno-sign-compare"),
-        extension("pddatetime.py", True, "-Wno-unreachable-code"),
-        extension("pydatetime.py", True, "-Wno-unreachable-code", "-Wno-sign-compare"),
+        extension("delta.py", True, "-Wno-unreachable-code"),
+        extension("parser.py", True, "-Wno-unreachable-code"),
+        extension("pddt.py", True, "-Wno-unreachable-code"),
+        extension("pydt.py", True, "-Wno-unreachable-code"),
         extension("typeref.py", False, "-Wno-unreachable-code"),
+        extension("utils.py", True, "-Wno-unreachable-code"),
     ]
 
 # Build
